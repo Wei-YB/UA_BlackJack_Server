9c3ff49cf50fc3c725b2c80020caff2ebded615d
aaronlv <aaronlv@tencent.com>
2021-06-18 17:10:17 +0800

    更新了demo, 以及添加说明

---
+++ gRPC_demo/cmake/build/CMakeFiles/client-asyn.dir/DependInfo.cmake
@@ -0,0 +1,20 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/root/gRPC_demo/client-asyn.cc" "CMakeFiles/client-asyn.dir/client-asyn.cc.o" "gcc" "CMakeFiles/client-asyn.dir/client-asyn.cc.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  "/root/gRPC_demo/cmake/build/CMakeFiles/hw_grpc_proto.dir/DependInfo.cmake"
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ gRPC_demo/cmake/build/CMakeFiles/server-syn.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/server-syn.dir/server-syn.cc.o"
+  "CMakeFiles/server-syn.dir/server-syn.cc.o.d"
+  "server-syn"
+  "server-syn.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/server-syn.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
